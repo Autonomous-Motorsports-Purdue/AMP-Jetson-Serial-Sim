af34767aa75df391122f5f819457c16aeafd58ae
Mitch <mitchaarndt@gmail.com>
2020-09-10 10:43:27 -0400

    switched approach to building on pixel uart

--- public/js/socket.js
+++
@@ -1,6 +0,0 @@
-const socket = io();
-socket.on('test', function (val) {
-	$('textarea#serial_input').val(val);
-	// $('#serial_input').append($('<li>').text(val));
-	console.log(val);
-});
--- server.js
+++ server.js
@@ -7,37 +7,85 @@
         --not sure if you need the above--
 
         to send:
-            echo -ne 'message' > /dev/ttyS5
+            ehco -ne 'message' > /dev/ttyS5
 
         to listen:
             cat -v < /dev/ttyS5
 */
+
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
 const app = express();
+const server = app.listen(3000);
 
-// routers
-const indexRouter = require('./routes/index');
-const bodyParser = require('body-parser');
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ limit: '1mb', extended: false }));
-
-app.use('/', indexRouter);
-
-server = app.listen(process.env.PORT || 3000);
+console.log('My socket server is running');
 
 const socket = require('socket.io');
 const io = socket(server);
+io.on('connection', newConnection);
 
-io.on('connection', (socket) => {
-	console.log(`new connection: ${socket.id}`);
-	socket.on('disconnect', () => {
-		console.log(`${socket.id} disconnected`);
-	});
-	socket.emit('test', 'testvalue');
-});
+// const SerialPort = require("serialport");
+// const port = new SerialPort(
+// 	"/dev/ttyS7",
+// 	{
+// 		BaudRate: 9600,
+// 	},
+// 	false //this is the openImmediately flag [default is true]
+// );
+
+// port.on("error", function (err) {
+// 	console.log("mitch error");
+// });
+
+// port.open(function (err) {
+// 	if (err) {
+// 		console.log(err);
+// 		return;
+// 	}
+// 	console.log("open");
+// 	port.on("data", function (data) {
+// 		console.log("data recieved: " + data);
+// 	});
+// 	port.write("ls\n", function (err, results) {
+// 		console.log("err" + err);
+// 		console.log("results " + results);
+// 	});
+// });
+
+function newConnection(socket) {
+	console.log('new connection:' + socket.id);
+
+	socket.on('img', imgMsg);
+
+	function imgMsg(data) {
+		console.log('recieved: ');
+		console.log(data.string);
+		// console.log(data.array);
+		data.array.forEach((element) => {
+			console.log('sending: ' + element);
+			port.write(element.toString());
+		});
+	}
+}
+
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort(
+	'/dev/ttyS6',
+	function (err) {
+		if (err == null) {
+			console.log('Connected to Serial Port');
+		} else {
+			console.log(err);
+		}
+	},
+	{ baudRate: 9600 }
+);
+
+const parser = new Readline();
+port.pipe(parser);
+
+parser.on('data', (line) => console.log(`> ${line}`));
+// > ROBOT ONLINE
+
+// setInterval(() => port.write("test"), 1000);
